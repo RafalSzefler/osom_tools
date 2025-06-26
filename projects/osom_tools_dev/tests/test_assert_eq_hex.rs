1c720cc0018ea70332a5568557ff69446f8a3e7b
Rafal Szefler <rafael.szefler@gmail.com>
2025-06-26 15:08:26 +0200

    Added asserts

--- projects/osom_tools_dev/tests/test_assert_eq_hex.rs
+++ projects/osom_tools_dev/tests/test_assert_eq_hex.rs
@@ -1,4 +1,4 @@
-use osom_tools_dev::macros::assert_eq_hex;
+use osom_tools_dev::macros::{assert_eq_hex, assert_neq_hex};
 
 #[test]
 fn test_assert_eq_hex() {
@@ -15,4 +15,21 @@ fn test_not_assert_eq_hex() {
     let b = [0x05];
 
     assert_eq_hex!(a, b);
+}
+
+#[test]
+fn test_assert_neq_hex() {
+    let a = [0x01, 0x02, 0x03, 0x04];
+    let b = [0x05];
+
+    assert_neq_hex!(a, b);
+}
+
+#[test]
+#[should_panic]
+fn test_not_assert_neq_hex() {
+    let a = [0x01, 0x02, 0x03, 0x04];
+    let b = [0x01, 0x02, 0x03, 0x04];
+
+    assert_neq_hex!(a, b);
 }
--- projects/osom_tools_macros/src/macros/assert_eq_hex.rs
+++ projects/osom_tools_macros/src/macros/assert_eq_hex.rs
@@ -1,3 +1,9 @@
+/// Compares two operands by equality. If operands are not equal panics
+/// and prints both operands as hex arrays.
+///
+/// # Arguments
+///
+/// Both arguments have to be arrays, slices or vectors.
 #[macro_export]
 macro_rules! assert_eq_hex {
     ($left:expr, $right:expr) => {{
@@ -8,6 +14,26 @@ macro_rules! assert_eq_hex {
                 r#"assertion `left == right` failed
   left: {left_expr}
  right: {right_expr}"#
+            )
+        }
+    }};
+}
+
+/// Compares two operands by equality. If operands are equal panics
+/// and prints a single operand as hex array.
+///
+/// # Arguments
+///
+/// Both arguments have to be arrays, slices or vectors.
+#[macro_export]
+macro_rules! assert_neq_hex {
+    ($left:expr, $right:expr) => {{
+        let left_expr = $crate::traits::AsDisplayAsHex::as_display_as_hex(&($left));
+        let right_expr = $crate::traits::AsDisplayAsHex::as_display_as_hex(&($right));
+        if left_expr.as_slice() == right_expr.as_slice() {
+            panic!(
+                r#"assertion `left != right` failed
+  value: {left_expr}"#
             )
         }
     }};
